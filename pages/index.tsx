e07f545d7eae3256bef64dcca3385ad5f55d130b
goya <goyasho0831@gmail.com>
2019-12-04 14:28:35 +0900

    絶対パスでインポートできるようにした

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { NextPage } from 'next';
+import Hello from '@src/components/Hello';
 
 const IndexPage: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <h1>Hello world! - useragent: {userAgent}</h1>
+  <div>
+    <h1>Hello world! - useragent: {userAgent}</h1>
+    <Hello />
+  </div>
 );
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
---
+++ src/components/Hello.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Hello: React.FC = () => <h1>HelloWorld!</h1>;
+
+export default Hello;
