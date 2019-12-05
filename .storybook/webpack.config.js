1f1e3fb5d7faa36cd123c24e4cb7cf9938612521
goya <goyasho0831@gmail.com>
2019-12-05 19:13:59 +0900

    webpack.config を更新した

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,12 @@
-const path = require('path');
+// TODO: 見直しは必要.
+const path = require("path");
+const SRC_PATH = path.join(__dirname, '../src');
 
 module.exports = ({ config, mode }) => {
   config.module.rules.push(
     {
       test: /\.stories\.[tj]sx?$/,
+      include: [SRC_PATH],
       use: [
         {
           loader: require.resolve('@storybook/source-loader'),
@@ -13,17 +16,16 @@ module.exports = ({ config, mode }) => {
           loader: require.resolve('react-docgen-typescript-loader'),
         },
       ],
-      // include: [path.resolve(__dirname, '../src')],
-      // enforce: 'pre',
     },
     {
       test: /\.(ts|tsx)$/,
+      include: [SRC_PATH],
       loader: require.resolve('babel-loader'),
       options: {
         presets: [['react-app', { flow: false, typescript: true }]],
       },
     }
-  )
-  config.resolve.extensions.push('.ts', '.tsx')
-  return config
-}
+  );
+  config.resolve.extensions.push('.js', '.jsx', '.ts', '.tsx');
+  return config;
+};
