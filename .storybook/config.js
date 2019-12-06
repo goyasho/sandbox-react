dd9f606c14de6e0183e4c059f421d22e28a52900
goya <goyasho0831@gmail.com>
2019-12-06 10:53:27 +0900

    storybook の設定を更新した

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,17 +1,22 @@
-import { configure, addDecorator } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
 import { setConsoleOptions } from "@storybook/addon-console";
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 setConsoleOptions({
   panelExclude: []
 });
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
 
 const req = require.context('../src', true, /.stories.(tsx|js)$/);
-
 function loadStories() {
   addDecorator(withInfo);
-  addDecorator(withKnobs);
+  addDecorator(withKnobs({ escapeHTML: false })); // & とかをエスケープしないようにしたいなら true にする.
 
   req.keys().forEach(req);
 }
